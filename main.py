953ba84c11af40e9f94fae38e9b9e8e24d3a60ed
Noah Snider <nsnider0@gmail.com>
2021-02-24 19:23:04 -0500

    refactor get_schedule()

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
-import math
+
 
 def get_schedule():
     url = 'https://www.basketball-reference.com/leagues/NBA_2021_games-february.html'
@@ -24,17 +24,16 @@ def get_schedule():
             try:
                 value = cell.get_text()
                 key = cell.get_attribute_list('data-stat')[0]
-            except AttributeError:
-                continue
+                df_cells[key] = value
 
-            df_cells[key] = value
+            except AttributeError:
+                pass
 
         df_rows.append(df_cells)
 
     driver.quit()
 
     df = pd.DataFrame(df_rows)
-    df = df[(df.date_game != 'Date')]
     return df
 
 
@@ -47,27 +46,18 @@ def get_four_factors():
     table = BeautifulSoup(html, 'html.parser').find(id='div_misc_stats')
 
     df_rows = []
+
     # loop through each row in table
     for row in table.find('tbody').find_all('tr'):
+
+        # initilize dict to hold row
         df_cells = dict()
 
         # loop through each cell in each row
         for cell in row.find_all('td'):
-
-            # if the cell has a hyperlink (e.g. for team name), there is a child element we need to check
-            if len(cell.findChildren()) != 0:
-                # column name
-                key = cell.get_attribute_list('data-stat')[0]
-                # cell value
-                value = cell.a.get_text()
-                # add to dictionary as key value pair
-                df_cells[key] = value
-
-            # if not, we can simply take the contents of the cell
-            else:
-                key = cell.get_attribute_list('data-stat')[0]
-                value = cell.get_text()
-                df_cells[key] = value
+            key = cell.get_attribute_list('data-stat')[0]
+            value = cell.get_text()
+            df_cells[key] = value
 
         df_rows.append(df_cells)
 
@@ -75,5 +65,7 @@ def get_four_factors():
     return pd.DataFrame(df_rows).dropna()
 
 
-four_facts = get_four_factors()
-schedule = get_schedule()
+if __name__ == "__main__":
+    four_facts = get_four_factors()
+    schedule = get_schedule()
+    print(four_facts)
